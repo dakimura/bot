034a3e0ff34c96d763d17fc359611aca6ff6f1a9
Daito Akimura <dakimura@mercari.com>
2022-10-11 09:06:26 +0900

    fix

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 
 sudo apt install pip
 pip install -r requirements.txt
-ps -ef | grep "streamlit" | grep -v grep | awk '{print $2}' | xargs kill
+pgrep streamlit | sudo xargs kill
 sudo nohup streamlit run --server.port=80 src/bot.py >> bot.log 2>&1 &
