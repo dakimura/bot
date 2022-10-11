710570ca7805d1004b59ae3b49e8b7423601893a
Daito Akimura <dakimura@mercari.com>
2022-10-11 09:04:50 +0900

    fix

--- restart.sh
+++ restart.sh
@@ -2,8 +2,5 @@
 
 sudo apt install pip
 pip install -r requirements.txt
-PID=$(ps -ef | grep "streamlit" | grep -v grep | awk '{print $2}')
-if [ -n "$PID" ]; then
-  sudo kill "$PID"
-fi
+ps -ef | grep "streamlit" | grep -v grep | awk '{print $2}' | xargs kill
 sudo nohup streamlit run --server.port=80 src/bot.py >> bot.log 2>&1 &
