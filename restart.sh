4fd0d8d0a404d2a13e45d1455942dbbde3bb6cf7
Daito Akimura <dakimura@mercari.com>
2022-10-23 15:29:02 +0900

    fix

--- restart.sh
+++ restart.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install pip
-pip install -r requirements.txt
+ALPACA_API_KEY_ID=$1
+ALPACA_API_SECRET_KEY=$2
+
+sudo apt install pip3
+pip3 install -r requirements.txt
 pgrep streamlit | sudo xargs kill
 pgrep bot | sudo xargs kill
 sudo nohup streamlit run --server.port=80 src/dashboard.py >>dashboard.log 2>&1 &
-sudo nohup python src/bot.py >>bot.log 2>&1 &
+ALPACA_API_KEY_ID=$ALPACA_API_KEY_ID ALPACA_API_SECRET_KEY=$ALPACA_API_SECRET_KEY nohup python3 src/bot.py >>bot.log 2>&1 &
